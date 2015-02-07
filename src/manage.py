fea1b3d428d85b19692a301f1b9cde23973b0497
fanchao-meng <fanchao.meng@rea-group.com>
2015-02-08 00:31:35 +0800

    增加pep8检查和修正相关的包

--- src/manage.py
+++ src/manage.py
@@ -9,13 +9,20 @@ from src import app
 from src import manager
 from src import db
 from src.models import *
+
+
 @manager.command
 def hello():
     print("hello")
-    
+
+
 @manager.command
 def create_users():
-    new_user = User(name='jesse', password='test', email='hello0@world.com', role='admin')
+    new_user = User(
+        name='jesse',
+        password='test',
+        email='hello0@world.com',
+        role='admin')
     db.session.add(new_user)
     new_user = User(name='lucy', password='test', email='hello1@world.com')
     db.session.add(new_user)
@@ -24,11 +31,12 @@ def create_users():
     new_user = User(name='marry', password='test', email='hello3@world.com')
     db.session.add(new_user)
     db.session.commit()
-    
+
+
 @manager.command
 def run():
     port = int(os.environ.get('PORT', 4444))
-    app.run(host='0.0.0.0', port=port) 
-       
+    app.run(host='0.0.0.0', port=port)
+
 if __name__ == "__main__":
     manager.run()
